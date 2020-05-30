f133ea9692d70614ce5533cdca1500d2b9ca7cba
Dieinimy <dieinimy@gmail.com>
2020-05-30 08:53:12 -0300

    Iniciando criação da tela home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,30 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 200,
+          child: Image.asset('images/810528Txcuexso.gif'),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black,
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Text('0'),
+          ),
+          IconButton(
+            icon: Icon(Icons.star),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
